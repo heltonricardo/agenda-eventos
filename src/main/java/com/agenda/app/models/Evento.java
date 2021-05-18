fcbadbbc082ae81735241ab4333c01897a6f9d31
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-17 22:56:36 -0300

    Update Evento.java

--- src/main/java/com/agenda/app/models/Evento.java
+++ src/main/java/com/agenda/app/models/Evento.java
@@ -1,11 +1,32 @@
 package com.agenda.app.models;
 
-public class Evento {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
 	private String nome;
 	private String local;
 	private String data;
 	private String horario;
+
+	public long getId() {
+		return codigo;
+	}
+
+	public void setId(long id) {
+		this.codigo = id;
+	}
 
 	public String getNome() {
 		return nome;
