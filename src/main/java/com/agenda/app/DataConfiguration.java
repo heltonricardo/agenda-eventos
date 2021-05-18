94255cb6c1553456d05c6965d1fe383cd11ae4ec
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-17 22:56:50 -0300

    conexao com o banco de dados

---
+++ src/main/java/com/agenda/app/DataConfiguration.java
@@ -0,0 +1,35 @@
+package com.agenda.app;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/agenda_eventos");
+		dataSource.setUsername("root");
+		dataSource.setPassword("12345678");
+		return dataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+}
