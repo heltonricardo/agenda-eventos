05c3f38ed8b03dc5a805c532d7bc463c36874703
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-19 14:18:38 -0300

    correcao de erro do driver do mysql

--- src/main/java/com/agenda/app/DataConfiguration.java
+++ src/main/java/com/agenda/app/DataConfiguration.java
@@ -15,7 +15,7 @@ public class DataConfiguration {
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/agenda_eventos");
 		dataSource.setUsername("root");
 		dataSource.setPassword("12345678");
