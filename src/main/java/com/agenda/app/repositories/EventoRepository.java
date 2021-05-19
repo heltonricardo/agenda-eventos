cb4dd1561b1513ec610d73d4f7428cd15367913a
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-19 17:45:09 -0300

    Update EventoRepository.java

--- src/main/java/com/agenda/app/repositories/EventoRepository.java
+++ src/main/java/com/agenda/app/repositories/EventoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.agenda.app.models.Evento;
 
 public interface EventoRepository extends CrudRepository<Evento, Long> {
-
+	Evento findByCodigo(long codigo);
 }
