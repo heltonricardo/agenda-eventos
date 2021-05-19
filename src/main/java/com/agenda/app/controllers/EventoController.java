4b0cd3652c00808aa0db9efc82f7c314e619e0eb
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-19 14:20:00 -0300

    persistindo dados no banco

--- src/main/java/com/agenda/app/controllers/EventoController.java
+++ src/main/java/com/agenda/app/controllers/EventoController.java
@@ -1,13 +1,27 @@
 package com.agenda.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.agenda.app.models.Evento;
+import com.agenda.app.repositories.EventoRepository;
 
 @Controller
 public class EventoController {
+	
+	@Autowired
+	private EventoRepository eventoRepository;
 
-	@RequestMapping("/cadastrarEvento")
+	@GetMapping("/cadastrarEvento")
 	public String form() {
 		return "evento/formEvento";
+	}
+	
+	@PostMapping("/cadastrarEvento")
+	public String form(Evento evento) {
+		eventoRepository.save(evento);
+		return "redirect:/cadastrarEvento";
 	}
 }
---
+++ src/main/java/com/agenda/app/repositories/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.agenda.app.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.agenda.app.models.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, Long>{
+
+}
