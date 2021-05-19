a30da3c26865ceb4e4c69f398f728551067405f4
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-19 17:46:31 -0300

    criacao de requisicoes especificas e pagina de detalhes

--- src/main/java/com/agenda/app/controllers/EventoController.java
+++ src/main/java/com/agenda/app/controllers/EventoController.java
@@ -3,6 +3,7 @@ package com.agenda.app.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,6 +32,14 @@ public class EventoController {
 		Iterable<Evento> eventos = eventoRepository.findAll();
 		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("eventos", eventos);
+		return mv;
+	}
+
+	@GetMapping("/detalhesEvento/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable long codigo) {
+		Evento evento = eventoRepository.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento", evento);
 		return mv;
 	}
 }
