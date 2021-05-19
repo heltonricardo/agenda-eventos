2e709789703339422bd90487c59d8943a20ff22b
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-19 14:32:20 -0300

    buscando e exibindo dados do banco

--- src/main/java/com/agenda/app/controllers/EventoController.java
+++ src/main/java/com/agenda/app/controllers/EventoController.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.agenda.app.models.Evento;
 import com.agenda.app.repositories.EventoRepository;
 
 @Controller
 public class EventoController {
-	
+
 	@Autowired
 	private EventoRepository eventoRepository;
 
@@ -18,10 +19,18 @@ public class EventoController {
 	public String form() {
 		return "evento/formEvento";
 	}
-	
+
 	@PostMapping("/cadastrarEvento")
 	public String form(Evento evento) {
 		eventoRepository.save(evento);
 		return "redirect:/cadastrarEvento";
+	}
+
+	@GetMapping("/eventos")
+	public ModelAndView listaEventos() {
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("eventos", eventos);
+		return mv;
 	}
 }
