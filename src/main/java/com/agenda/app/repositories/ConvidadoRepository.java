b13ee8d79ac458c4f2e9d7ad13286e830eb833c0
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-23 19:58:28 -0300

    Update ConvidadoRepository.java

--- src/main/java/com/agenda/app/repositories/ConvidadoRepository.java
+++ src/main/java/com/agenda/app/repositories/ConvidadoRepository.java
@@ -3,6 +3,8 @@ package com.agenda.app.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.agenda.app.models.Convidado;
+import com.agenda.app.models.Evento;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado, Long> {
+	Iterable<Convidado> findByEvento(Evento evento);
 }
